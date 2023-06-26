a7d70d7b6e79dc9ccf659240c9c5191a8b0475db
yanwang.lj <yanwang.lj@antgroup.com>
2023-06-26 10:52:11 +0800

    fix: relative path

--- blog/scripts/cdn.filter.js
+++ blog/scripts/cdn.filter.js
@@ -3,15 +3,15 @@ hexo.extend.filter.register('after_render:html', function (data) {
   if (process.env.NODE_ENV !== 'ci') {
     // 本地环境修正图片路径，原始地址确保在 Github 下展示页正常
     return data.replace(/<img(.*?)src="(.*?)"(.*?)>/gi, function (str, p1, p2, p3) {
-      if (/^\/\.\.\/blogimgs\//.test(p2)) return str.replace(p2, p2.slice(3));
-      return str.replace(p3, p3 + ' loading="lazy"');
+      if (/^\/\.\.\/blogimgs\//.test(p2)) return str.replace(p2, p2.slice(3)).replace(p3, 'loading="lazy" ' + p3);
+      return str;
     });
   }
   // Github Action 的 CI 环境才执行 CDN 替换
   const cdn = this.config.assets_cdn;
   return data.replace(/<img(.*?)src="(.*?)"(.*?)>/gi, function (str, p1, p2, p3) {
-    if (/^\/\.\.\/blogimgs\//.test(p2)) return str.replace(p2, cdn + p2.slice(3));
-    return str.replace(p3, p3 + ' loading="lazy"');
+    if (/^\/\.\.\/blogimgs\//.test(p2)) return str.replace(p2, cdn + p2.slice(3)).replace(p3, 'loading="lazy" ' + p3);
+    return str;
   }).replace(/<script(.*?)src="(.*?)"/gi, function (str, p1, p2) {
     if (/^\/public\/js\//.test(p2)) return str.replace(p2, cdn + p2);
     return str;
