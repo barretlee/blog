30e6eabf4cd7b126fdc7602c702b2b18fe879466
yanwang.lj <yanwang.lj@antgroup.com>
2023-06-26 09:56:17 +0800

    fix: cdn address

--- scripts/resolve-content.js
+++ scripts/resolve-content.js
@@ -25,7 +25,6 @@ function deal(file) {
   var IMG_REG = /\!\[([^\]]+?)?\]\(([\s\S]+?)\)/g;
   var START_REG = /^[\s\n]*---\s*\n/m;
   var content = fs.readFileSync(file).toString();
-  var LOADING_IMG_URL = "https://img.alicdn.com/tfs/TB1oyqGa_tYBeNjy1XdXXXXyVXa-300-300.png";
   var isStandardPath = true;
   var date = DATE_REG.exec(file);
   if (!date) {
@@ -42,15 +41,9 @@ function deal(file) {
     $2 = $2.split(' ')[0];
     // CDN 加速情况不处理
     if ($2.indexOf('img.alicdn.com') > -1 || $2.indexOf('cdn.jsdelivr.net') > -1) return $0;
-    if ($2 === LOADING_IMG_URL) return $0;
     if ($2.indexOf('www.barretlee.com') > -1) {
-      // console.log('rename', $2);
       var p = '/blogimgs/' + $2.split('/blogimgs/')[1];
       return `![${$1 || 'image'}](${p})`;
-    }
-    if (!/\.(jpg|png|svg|gif|jpeg)$/.test($2)) {
-      console.log(`unknown image type: ${$2}.`);
-      return $0;
     }
     var imgRoot = path.join(__dirname, `../blog/src/blogimgs/`);
     var imgDirPath = `${date[1]}/${date[2]}/${date[3]}`;
